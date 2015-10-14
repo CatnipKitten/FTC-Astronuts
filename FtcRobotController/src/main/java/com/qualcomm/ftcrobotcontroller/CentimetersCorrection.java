be94cc7c10b2ef85a06b8105211e725398317499
preescoot <prestomahon@gmail.com>
2015-10-14 16:13:43 -0700

    Class for turning Ultrasonic Value into Centimeters and Inches.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/CentimetersCorrection.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/CentimetersCorrection.java
@@ -6,9 +6,11 @@ import com.qualcomm.robotcore.hardware.UltrasonicSensor;
  * Created by Prescott on 10/14/15.
  */
 public class CentimetersCorrection {
+    // Makes doubles for the values.
     public double centimeters;
     public double inches;
 
+    //Creates Method for converting Ultrasonic Value to Centimeters and Inches.
     public void getcentimeters(UltrasonicSensor ultrasonicSensor) {
         double raw = ultrasonicSensor.getUltrasonicLevel();
         centimeters = raw - 3;
