3590a6761e76fe7b09433306a83b96fa9a75e84e
preescoot <prestomahon@gmail.com>
2015-10-14 16:13:59 -0700

    Class for turning Ultrasonic Value into Centimeters and Inches.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/UltrasonicSensorTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/UltrasonicSensorTest.java
@@ -7,16 +7,20 @@ import com.qualcomm.robotcore.hardware.UltrasonicSensor;
  * Created by Prescott on 10/7/15.
  */
 public class UltrasonicSensorTest extends OpMode {
+    //Makes variable for the Ultrasonic Sensor.
     UltrasonicSensor ultrasonicsensor;
 
     @Override
     public void init () {
+        //Maps the Ultrasonic Sensor.
         ultrasonicsensor = hardwareMap.ultrasonicSensor.get("ultrasonic_sensor");
     }
     public void loop () {
+        //Corrects the Raw data from the Ultrasonic Sensor.
         double ultrasonic = ultrasonicsensor.getUltrasonicLevel();
         double ultracorrected = ultrasonic - 3;
 
+        //Displays the Data.
         telemetry.addData("4-ultrasonic_sensor", ultrasonic);
         telemetry.addData("Ultrasonic Corrected", ultracorrected);
     }
