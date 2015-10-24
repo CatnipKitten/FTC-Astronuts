116ec9f50c532f42cd0b5f5be4e7415be9d4f2ec
DawsonFranz <dawsonfranz@gmail.com>
2015-10-24 14:34:45 -0700

    Created K9LinearOP - Gets and reads values for color and ultrasonic sensor. Need to make it push a button...

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -97,5 +97,6 @@ public class FtcOpModeRegister implements OpModeRegister {
     */
     
     manager.register("TeleOP", teleop.class);
+    manager.register("K9LinearOp", K9LinearOp.class);
   }
 }
